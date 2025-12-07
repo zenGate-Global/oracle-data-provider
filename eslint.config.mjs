49929abae7a737e2ece101c72ec91bd26bb71c38
Shishir Pai <64056966+mgpai22@users.noreply.github.com>
2025-12-06 20:32:09 -0500

    fix: updated for tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import baseConfig from '@hono/eslint-config'
 
 export default [
+  {
+    ignores: ['eslint.config.mjs'],
+  },
   ...baseConfig,
   {
     languageOptions: {
@@ -8,6 +11,16 @@ export default [
         project: './tsconfig.json',
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+  },
+  {
+    files: ['test/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      'import-x/consistent-type-specifier-style': 'off',
     },
   },
 ]
